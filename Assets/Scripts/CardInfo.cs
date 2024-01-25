43640f5b8f6e170e71650e4268630b8144df4569
juliorkm <julioramakrsnamandoju@gmail.com>
2024-01-25 03:01:30 -0300

    Fix card zoom

--- Assets/Scripts/CardInfo.cs
+++ Assets/Scripts/CardInfo.cs
@@ -6,15 +6,18 @@ using UnityEngine.UI;
 public class CardInfo : MonoBehaviour {
     [HideInInspector]
     public VirtualTransform vt;
+    private RectTransform rt;
     private Vector3 hiddenPosition;
     private Vector3 publicPosition;
     private Image image;
+    private bool isOpen = false;
 
     // Start is called before the first frame update
     void Start() {
         vt = GetComponent<VirtualTransform>();
-        hiddenPosition = transform.position;
-        publicPosition = hiddenPosition + new Vector3(500, 0, 0);
+        rt = GetComponent<RectTransform>();
+        hiddenPosition = new Vector3(-300, 0, 0);
+        publicPosition = new Vector3(500, 0, 0);
         image = GetComponent<Image>();
     }
 
@@ -23,11 +26,19 @@ public class CardInfo : MonoBehaviour {
     }
 
     public void Close() {
-        vt.position = hiddenPosition;
+        var currentPosition = rt.anchoredPosition;
+        rt.anchoredPosition = hiddenPosition;
+        vt.position = transform.position;
+        rt.anchoredPosition = currentPosition;
+        isOpen = false;
     }
 
     public void Open(Sprite sprite) {
         image.sprite = sprite;
-        vt.position = publicPosition;
+        var currentPosition = rt.anchoredPosition;
+        rt.anchoredPosition = publicPosition;
+        vt.position = transform.position;
+        rt.anchoredPosition = currentPosition;
+        isOpen = true;
     }
 }
